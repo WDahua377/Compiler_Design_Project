823a8eaef62cea75b4a7327ed342147ac6606dba
408410075 <danmei891120@gmail.com>
2024-10-10 16:08:26 +0800

    Testing program

---
+++ project4_v1/sample1.c
@@ -0,0 +1,10 @@
+int main(){
+   int a;
+   a = 1;
+   if(a < 1){
+      printf("a == 2:");
+   }
+   else{
+      printf ( "Yes, a == %d" , a ) ;
+   }
+}
---
+++ project4_v1/sample3.c
@@ -0,0 +1,11 @@
+int main(){
+   int a;
+   int i;
+   int b;
+    if(a == 1){
+        i *= 2;
+    }
+    else{
+        i = i + 1;
+    }
+}
